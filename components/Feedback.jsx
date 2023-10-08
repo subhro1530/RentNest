8a8b6a5607e437333cb116af23c8715cd0d0e25a
Jaimin Hapani <jaiminhapani7787@gmail.com>
2023-10-08 20:45:09 +0530

    "correct spelling mistake"

--- components/Feedback.jsx
+++ components/Feedback.jsx
@@ -6,7 +6,7 @@ function Feedback() {
     {
       id: 1,
       userName: "Jaimin Hapani",
-      role: "Satyed @ BTM Layout",
+      role: "Stayed @ BTM Layout",
       imageUrl:
         "https://business.rentnest.in/restnest/static/media/ps.25900b0e9380815b1999.jpg",
       msg: "I cannot wait for RentNest to launch! The idea of a platform that simplifies room and flat hunting in Bangalore is exactly what we need. I am excited to see how RentNest will make our lives easier.",
@@ -14,14 +14,14 @@ function Feedback() {
     {
       id: 2,
       userName: "Ajay Asnani",
-      role: "Satyed @ Old Airport Road",
+      role: "Stayed @ Old Airport Road",
       imageUrl: "https://demo.rentnest.in/images/Ajay4.JPG",
       msg: "I've been following RentNest's development closely, and I'm impressed with the dedication and innovation they're bringing to the table. I have high hopes that RentNest will revolutionize the way we rent properties in Bangalore.",
     },
     {
       id: 3,
       userName: "Shaswata Saha",
-      role: "Satyed @ Bellandur",
+      role: "Stayed @ Bellandur",
       imageUrl: "https://demo.rentnest.in/images/shas.jpg",
       msg: "As someone currently searching for rental options in Bangalore, I'm eagerly looking forward to RentNest's launch. The concept of a one-stop platform for finding rooms, PGs, and flats sounds like a game-changer.",
     },
