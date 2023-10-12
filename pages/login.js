b11c031e2aac1a93a0c77aa1f4daf43a8b82b973
Shaswata Saha <shaswata.ssaha@gmail.com>
2023-10-12 20:50:53 +0530

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -1,1 +1,28 @@
+import LoginComponent from "../components/login"
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+import SecondNavbar from "../components/secondnavbar";
+import React from 'react'
+import styles from './styles.module.css'
+import { useMediaQuery } from "@mui/material";
 
+
+const Login = () => {
+  const matches = useMediaQuery('(min-width:630px)');
+  const matches2 = useMediaQuery('(min-width:770px)');
+  return (
+    <>
+      <div className={styles.hero} style={{
+        alignItems: matches ? "unset" : "center", padding: matches ? "1rem" : "0px",
+        height: matches2 ? "auto" : "80vh",
+      justifyContent:matches2?"center":"space-around"}}>
+      <Navbar topic="SignUp"/>
+    
+     <LoginComponent/>
+     </div>
+      <Footer />
+      </>
+  );
+}
+
+export default Login
